1d55ff1b03966d1185bafdd3dc638a1d2f532ec8
wolfpro231 <48035876+wolfpro231@users.noreply.github.com>
2019-05-09 15:45:17 +0200

    Add files via upload

---
+++ bc.js
@@ -0,0 +1,18 @@
+﻿const Discord = require("discord.js");
+const client = new Discord.Client();
+
+
+client.on('ready', () => {
+   console.log(`----------------`);
+      console.log(`Desert Bot- Script By : Darks`);
+        console.log(`----------------`);
+      console.log(`ON ${client.guilds.size} Servers '     Script By :Darks  ' `);
+    console.log(`----------------`);
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(`BesO Here`,"http://twitch.tv/S-F")
+client.user.setStatus("dnd")
+});
+
+
+
+client.login("");
